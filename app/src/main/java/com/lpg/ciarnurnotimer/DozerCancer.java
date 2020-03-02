2dd8c1f502eba15ef8c69b7f4f28187052079566
dosse91 <info@fdossena.com>
2020-03-02 19:20:18 +0100

    Debotnet and typo fixes

--- app/src/main/java/com/lpg/ciarnurnotimer/DozerCancer.java
+++ app/src/main/java/com/lpg/ciarnurnotimer/DozerCancer.java
@@ -4,13 +4,8 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.IBinder;
-import android.util.Log;
-
-import androidx.core.app.NotificationCompat;
 
 public class DozerCancer extends Service {
 
