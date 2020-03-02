fec265110fa9a70b7e94eb6f4f2076906d6441e2
e-magon <ahabhyde@gmail.com>
2020-03-02 18:45:39 +0100

    Foreground service notification (tapping on it not yet implemented)

---
+++ app/src/main/java/com/lpg/ciarnurnotimer/DozerCancer.java
@@ -0,0 +1,73 @@
+package com.lpg.ciarnurnotimer;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+public class DozerCancer extends Service {
+
+    public static final String ACTION_START_FOREGROUND_SERVICE = "ACTION_START_FOREGROUND_SERVICE";
+    public static final String ACTION_STOP_FOREGROUND_SERVICE = "ACTION_STOP_FOREGROUND_SERVICE";
+
+    public DozerCancer() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+
+            switch (action) {
+                case ACTION_START_FOREGROUND_SERVICE:
+                    startForegroundService();
+                    break;
+                case ACTION_STOP_FOREGROUND_SERVICE:
+                    stopForegroundService();
+                    break;
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    /* Used to build and start foreground service. */
+    private void startForegroundService() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+            Notification notification =
+                    new Notification.Builder(this, "1")
+                            .setContentTitle(getText(R.string.notification_title))
+                            .setContentText(getText(R.string.notification_message))
+                            .setSmallIcon(R.drawable.ic_launcher_foreground)
+                            .setContentIntent(pendingIntent)
+                            .build();
+
+            // Start foreground service.
+            startForeground(1, notification);
+        }
+    }
+
+    private void stopForegroundService() {
+        // Stop foreground service and remove the notification.
+        stopForeground(true);
+
+        // Stop the foreground service.
+        stopSelf();
+    }
+}
--- app/src/main/java/com/lpg/ciarnurnotimer/MainActivity.java
+++ app/src/main/java/com/lpg/ciarnurnotimer/MainActivity.java
@@ -3,24 +3,37 @@ package com.lpg.ciarnurnotimer;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.ValueCallback;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import im.delight.android.webview.AdvancedWebView;
 
 public class MainActivity extends Activity implements AdvancedWebView.Listener {
 
     private final int ACTIVITY_LAYOUT = R.layout.activity_main;
+    private final int TIMER_PERIOD = 500;
+
+    private boolean lastStatus = false; //last value of js variable leaveConfirmRequired
+
     private AdvancedWebView mWebView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(ACTIVITY_LAYOUT);
+
+        createNotificationChannel();
 
         mWebView = (AdvancedWebView) findViewById(R.id.webview);
 
@@ -28,6 +41,38 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
         mWebView.addPermittedHostname("");
 
         mWebView.loadUrl("file:///android_asset/index.html");
+
+        Timer persistenceTimer = new Timer();
+        persistenceTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWebView.evaluateJavascript("leaveConfirmRequired", new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String s) {
+                                if (s.equalsIgnoreCase("true")) {
+                                    if (!lastStatus) {
+                                        //Start foreground service
+                                        startForegroundService();
+
+                                        lastStatus = true;
+                                    }
+                                } else {
+                                    if (lastStatus) {
+                                        //Stop foreground service
+                                        stopForegroundService();
+
+                                        lastStatus = false;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        }, 1000, TIMER_PERIOD);
     }
 
     @SuppressLint("NewApi")
@@ -101,6 +146,8 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
 
+
+    // Misc methods
     private void showExitDialog() {
         AlertDialog.Builder exitDialog = new AlertDialog.Builder(this);
         exitDialog.setTitle("Sei sicuro di voler uscire dall'applicazione?");
@@ -116,6 +163,34 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
         exitDialog.setNegativeButton("Annulla", null);
         AlertDialog alert = exitDialog.create();
         alert.show();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= 26) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void startForegroundService() {
+        Intent intent = new Intent(MainActivity.this, DozerCancer.class);
+        intent.setAction(DozerCancer.ACTION_START_FOREGROUND_SERVICE);
+        startService(intent);
+    }
+
+    private void stopForegroundService() {
+        Intent intent = new Intent(MainActivity.this, DozerCancer.class);
+        intent.setAction(DozerCancer.ACTION_STOP_FOREGROUND_SERVICE);
+        startService(intent);
     }
 
 }
